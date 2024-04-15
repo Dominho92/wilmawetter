eb167e6e1f2586bdf19dfa367791e6d9b1e4604d
Domi <Domi>
2024-04-16 01:37:29 +0200

    test

--- lib/home/styles/simple_styles.dart
+++ lib/home/styles/simple_styles.dart
@@ -11,3 +11,8 @@ const cityTextStyle = TextStyle(
     color: Color.fromARGB(255, 1, 151, 171),
     fontSize: 16,
     fontWeight: FontWeight.bold);
+
+const weatherTextStyle = TextStyle(
+    color: Color.fromARGB(255, 42, 42, 42),
+    fontSize: 15,
+    fontWeight: FontWeight.bold);
